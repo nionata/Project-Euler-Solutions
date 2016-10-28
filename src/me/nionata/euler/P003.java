5cf01b0c0a69baa15e982a93598ea33829046981
Nicholas Ionata <nicktheskill@gmail.com>
2016-10-28 06:09:23 -0400

    Problems 1&2 done Problem 3 underway

--- src/me/nionata/euler/P003.java
+++ src/me/nionata/euler/P003.java
@@ -1,5 +1,17 @@
 package me.nionata.euler;
 
 public class P003 {
+	
+	public static int run() {
+		int num = 0;
+		
+		double i = 0;
+		
+		while (i < 600851475143.0) {
+			
+			
+		}
+		return num;
+	}
 
 }
